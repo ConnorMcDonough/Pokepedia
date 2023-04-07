699b92fd29339988ef3bb46d702e05a13d0842c7
ConnorMcDonough <connorandrewmcdonough@gmail.com>
2023-04-07 19:24:46 -0400

    fx

--- Pokepedia.Domain/Contenders/Pokemons/PokemonIdContender.cs
+++ Pokepedia.Domain/Contenders/Pokemons/PokemonIdContender.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Pokepedia.Domain.Contenders.Pokemons
+﻿namespace Pokepedia.Domain.Contenders.Pokemons
 {
     internal class PokemonIdContender
     {
